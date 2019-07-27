1f01e066db51b8ec0b238930feb7e7a7d7f2e664
Martha Troubh <indexer@icloud.com>
2019-07-27 01:57:25 -0600

    Spec model file for Course relationship and validations

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -1,1 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
+
+Rspec.describe Course do
+  describe 'Relationships' do
+    it { should have_many :student_courses }
+    it { should have_many(:students).through(:student_courses) }
+  end
+
+  describe 'Validations' do
+    it { should validate_presence_of :name }
+  end
+end
